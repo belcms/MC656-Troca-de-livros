416ff8c02f48e9ee78229589fb56a18209e1a962
Isabel Cristina <isabelmarras13@gmail.com>
2026-04-11 15:25:59 -0300

    configuracao para conectar flutter com back

---
+++ frontend/lib/services/user_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'api_client.dart'; // Importa a configuração base
+
+class UserService {
+  // Usa o baseUrl do ApiClient
+  static final String _usersUrl = '${ApiClient.baseUrl}/users';
+
+  static Future<List<dynamic>?> fetchUsers() async {
+    try {
+      final response = await http.get(Uri.parse(_usersUrl));
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body); // Retorna os dados brutos
+      } else {
+        print('Erro: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Erro de conexão: $e');
+      return null;
+    }
+  }
+}
