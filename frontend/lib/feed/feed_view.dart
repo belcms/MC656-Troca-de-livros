7c891d46b37d1131c6e3024a4b860f5561943c43
Isabel Cristina <isabelmarras13@gmail.com>
2026-04-12 00:08:18 -0300

    ajustes dos tamanhos e estilos do componente do card para o anúncio

--- frontend/lib/feed/feed_view.dart
+++ frontend/lib/feed/feed_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'announcement_card.dart';
 
 class FeedView extends StatelessWidget {
   const FeedView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("FEED");
+    return Center(
+      child: AnnouncementCard(title: 'Trono de vidro', publishYear: 2014, photo: 'https://m.media-amazon.com/images/I/81m94OedHqL._SY522_.jpg', cep: "Campinas - SP")
+    );
+    
   }
 }
