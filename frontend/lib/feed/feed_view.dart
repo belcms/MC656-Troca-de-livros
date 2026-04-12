0ae1f9932609a2a7b29f4f174dbaeedeb38fb05a
Isabel Cristina <isabelmarras13@gmail.com>
2026-04-12 01:43:16 -0300

    configura a cor de fundo e de acento do theme do app

--- frontend/lib/feed/feed_view.dart
+++ frontend/lib/feed/feed_view.dart
@@ -28,6 +28,7 @@ class FeedView extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
               child: Text('Home', style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontWeight: .bold)),
+              
               ),
 
 
