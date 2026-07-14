e1b87c9a1bd922f156cc21004b17b6a2b8c65f98
Kauan-Cunha <kauancunha6@gmail.com>
2026-07-14 17:48:48 -0300

    Refactor codebase by removing unused imports, commented code, and redundant files; streamline model and service definitions for improved clarity and maintainability.

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,25 +11,14 @@ from app.domain.locations import models as location_model
 
 from app.domain.announcements import models as announcements_models
 from app.domain.announcements.services import create_dummy_data
-# from app.domain.announcements.router import router as announcements_router
 from app.api.v1.announcements.router import router as announcements_router
-from app.core.database import engine, Base, get_db, ensure_schema_compatibility
+from app.core.database import engine, Base, get_db
 from app.api.v1.users.router import router as users_router
 from app.api.v1.books.router import router as books_router
 from app.api.v1.auth.router import router as auth_router
 from app.api.v1.offer.router import router_offer as offer_router
 from app.api.v1.offer.router import router_offer_announcement as offer_announcement_router
 from app.api.v1.locations.router import router as locations_router
-
-# books_models.Base.metadata.create_all(bind=engine)
-# users_models.Base.metadata.create_all(bind=engine)
-# announcements_models.Base.metadata.create_all(bind=engine)
-# offer_models.Base.metadata.create_all(bind=engine)
-
-#
-
-# Base.metadata.create_all(bind=engine)
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -42,13 +31,8 @@ async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
 
     yield
-    # Tudo que está depois do yield roda quando o servidor desligar
 
-# 2. Passe a função para o app e remova a linha que estava solta no arquivo
 app = FastAPI(lifespan=lifespan)
-
-
-# app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
